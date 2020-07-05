0cdfc2aa825beb93e3f9e4c88f52e04ee4a01d2d
demola07 <demolasobaki@gmail.com>
2020-07-05 23:29:35 +0100

    created user model and signup controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,29 @@
+const User = require('../models/User')
+
 exports.signUp = async (req, res, next) => {
-  console.log('req.body', req.body)
-  console.log('req.value.body', req.value.body)
-  console.log('SignUp route reached')
-  res.send('Signup working')
+  const { email, password } = req.value.body
+
+  // Check if user already exists
+  const foundUser = await User.findOne({ email })
+
+  if (foundUser) {
+    return res.status(403).json({
+      success: false,
+      error: 'User already exists'
+    })
+  }
+
+  // Create new user
+  const newUser = new User({ email, password })
+
+  const createdUser = await newUser.save()
+
+  // Respond with token
+  res.status(201).json({
+    success: true,
+    message: 'User created',
+    data: createdUser
+  })
 }
 exports.signIn = async (req, res, next) => {
   console.log('SignIn route reached')
